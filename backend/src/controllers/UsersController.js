da3a0cb809bb8a9f36e05e1eec0fadd2be6c9355
MarcsZERO <54442113+MarcsZERO@users.noreply.github.com>
2020-10-27 20:24:18 -0300

    Atualizando sqlite3, porém existe um erro no cadastro de usuár

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -26,7 +26,7 @@ module.exports = {
         }
     },
 
-    async index(rquest, response) {
+    async index(request, response) {
         const users = await connection('users').select('*');
 
         return response.json(users);
